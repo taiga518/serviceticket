8129e761cf35973168fb32766f8d85caa77cc739
taiga518 <taiga518@gmail.com>
2017-12-10 10:14:40 +0000

    [fix] incompatible /ascii/utf8 データベースの文字コードを変更したら直った

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+#    I18n.available_locales = [:en, :ja]
+#    I18n.enforce_available_locales = true
+#    I18n.default_locale = :ja
     I18n.enforce_available_locales = false
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
