f543a802db43a58c50bc458834efd081dc6942f2
taiga518 <taiga518@gmail.com>
2017-12-10 07:12:30 +0000

    [update] seedのitemのbodyを日本語にした

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@
   )
 end
 
-body_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non pulvinar sapien. Donec vel blandit eros, vel vestibulum tellus. Suspendisse imperdiet mattis eros, et luctus mi sagittis sit amet. In aliquam nisl a neque aliquam sodales. Morbi lacinia venenatis metus, id accumsan felis lacinia quis. Curabitur a nisl sodales, vestibulum augue non, eleifend quam. Maecenas dapibus ac elit eget commodo. Integer pulvinar, risus eu facilisis condimentum, arcu ante ultricies magna, et iaculis mi sem ac mauris. Nullam in diam ac."
+body_text = "吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。"
 
 5.times do |index|
   Item.create(
