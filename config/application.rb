3fcd0e3df8451c7ad13a01beff56dd9871c8df19
taiga518 <taiga518@gmail.com>
2017-12-10 09:33:28 +0000

    [fix] seed booking

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-  #  I18n.enforce_available_locales = true
-  #  config.i18n.default_locale = :ja
+    I18n.enforce_available_locales = false
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@
   )
 end
 
-body_text = "吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。"
+body_text = "aaa"
 
 5.times do |index|
   Item.create(
@@ -54,10 +54,11 @@ end
       gift_type: 1000*(j+1),
       message: body_text,
       delivery_method: 1,
-      to_email: "receiver_#{index+1}@email",
+      to_email: "receiver#{index+1}@email",
       from_name: "giver_#{index+1}",
-      from_email: "giver_#{index+1}@email",
-      serial: SecureRandom.hex(5)
+      from_email: "giver#{index+1}@email",
+      serial: SecureRandom.hex(5),
+      item_id: (index+1)
     )
     end
 end
